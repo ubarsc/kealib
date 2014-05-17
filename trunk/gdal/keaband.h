7228c73900b81b1a5f1bd3b4d2b59fe8a6581710
Sam Gillingham <gillingham.sam@gmail.com>
2014-05-17 17:16:42 +1200

    removed support for STATISTICS_HISTOBINVALUES. Use RAT to access histogram instead.

--- trunk/gdal/keaband.h
+++ trunk/gdal/keaband.h
@@ -118,11 +118,6 @@ protected:
     // updates m_papszMetadataList
     void UpdateMetadataList();
 
-    // functions for getting and setting the histogram as part of the 
-    // metadata - uses the RAT
-    void SetHistogramFromMetadata(const char *pszHistogram);
-    std::string GetHistogramAsMetadata();
-
     kealib::KEAImageIO  *m_pImageIO; // our image access pointer - refcounted
     char               **m_papszMetadataList; // CPLStringList of metadata
     kealib::KEADataType  m_eKEADataType; // data type as KEA enum
