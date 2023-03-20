c5b2395b16fe9cf67b22e49496dae77491e4c481
Sam Gillingham <gillingham.sam@gmail.com>
2023-03-20 14:18:58 +1000

    install fakegdal and do final fixups

--- python/testpython.py
+++ python/testpython.py
@@ -1,6 +1,36 @@
 #!/usr/bin/env python
 
 """
+ *  build_neighbours.py
+ *  LibKEA
+ *
+ *  Created by Sam Gillingham on 09/02/2023.
+ *  Copyright 2012 LibKEA. All rights reserved.
+ *
+ *  This file is part of LibKEA.
+ *
+ *  Permission is hereby granted, free of charge, to any person 
+ *  obtaining a copy of this software and associated documentation 
+ *  files (the "Software"), to deal in the Software without restriction, 
+ *  including without limitation the rights to use, copy, modify, 
+ *  merge, publish, distribute, sublicense, and/or sell copies of the 
+ *  Software, and to permit persons to whom the Software is furnished 
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be 
+ *  included in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+ *  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR 
+ *  ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+ *  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+ *  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+"""
+
+"""
 Test harness for kealib.extrat
 """
 
@@ -10,7 +40,7 @@ import awkward
 
 from kealib import extrat
 from kealib import build_neighbours
-import fakegdal
+from kealib import fakegdal
 
 INIT_RAT_SIZE = 100 # set in testseg.cpp
 CORRECT_NEIGHBOURS = {0: [1, 10], 1: [0, 11], 10: [0, 11], 11: [10, 1]}
