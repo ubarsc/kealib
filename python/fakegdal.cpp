c5a3ccc17dbf06f036b84520708a39bc17f41a12
Sam Gillingham <gillingham.sam@gmail.com>
2023-03-22 16:26:45 +1000

    go back to old style struct

--- python/fakegdal.cpp
+++ python/fakegdal.cpp
@@ -136,15 +136,46 @@ static PyGetSetDef fakeGDAL_getseters[] = {
     {NULL}  /* Sentinel */
 };
 
+// Incredibly, C99 style partial declerations aren't supported in C++!
 static PyTypeObject FakeGDALDataset_Type = {
     PyVarObject_HEAD_INIT(NULL, 0)
-    .tp_name = "fakegdal.Dataset",
-    .tp_basicsize = sizeof(FakeGDALObject),
-    .tp_dealloc = (destructor)fakeGDAL_dealloc,
-    .tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
-    .tp_doc = PyDoc_STR("Fake GDAL Dataset Object"),
-    .tp_getset = fakeGDAL_getseters,
-    .tp_init = (initproc)fakeGDAL_init
+    "fakegdal.Dataset",     // tp_name
+    sizeof(FakeGDALObject), // tp_basicsize
+    0,                      // tp_itemsize 
+    (destructor)fakeGDAL_dealloc, // tp_dealloc
+    0,                         /*tp_print*/
+    0,                         /*tp_getattr*/
+    0,                         /*tp_setattr*/
+    0,                         /*tp_compare*/
+    0,                         /*tp_repr*/
+    0,                         /*tp_as_number*/
+    0,                         /*tp_as_sequence*/
+    0,                         /*tp_as_mapping*/
+    0,                         /*tp_hash */
+    0,                         /*tp_call*/
+    0,                         /*tp_str*/
+    0,                         /*tp_getattro*/
+    0,                         /*tp_setattro*/
+    0,                         /*tp_as_buffer*/
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, // tp_flags
+    PyDoc_STR("Fake GDAL Dataset Object"), // tp_doc
+    0,                     /* tp_traverse */
+    0,                     /* tp_clear */
+    0,                     /* tp_richcompare */
+    0,                     /* tp_weaklistoffset */
+    0,                     /* tp_iter */
+    0,                     /* tp_iternext */
+    0,                      // tp_methods
+    0,                      // tp_members
+    fakeGDAL_getseters,     // tp_getset
+    0,                         /* tp_base */
+    0,                         /* tp_dict */
+    0,                         /* tp_descr_get */
+    0,                         /* tp_descr_set */
+    0,                         /* tp_dictoffset */
+    (initproc)fakeGDAL_init,     // tp_init
+    0,                         /* tp_alloc */
+    0                 /* tp_new */
 };
 
 static struct PyModuleDef FakeGDAL_Module = {
