750981591bcd22fb57ac8a61eeeeffbf741857a9
Sam Gillingham <gillingham.sam@gmail.com>
2025-09-14 08:17:12 +1000

    backport testsuite

---
+++ src/tests/testread.cpp
@@ -0,0 +1,362 @@
+/*
+ *  testread.cpp
+ *  LibKEA
+ *
+ *  Created by Pete Bunting on 02/07/2012.
+ *  Copyright 2012 LibKEA. All rights reserved.
+ *
+ *  This file is part of LibKEA.
+ *
+ *  Permission is hereby granted, free of charge, to any person 
+ *  obtaining a copy of this software and associated documentation 
+ *  files (the "Software"), to deal in the Software without restriction, 
+ *  including without limitation the rights to use, copy, modify, 
+ *  merge, publish, distribute, sublicense, and/or sell copies of the 
+ *  Software, and to permit persons to whom the Software is furnished 
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be 
+ *  included in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+ *  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR 
+ *  ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+ *  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+ *  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <algorithm>
+#include "libkea/KEAImageIO.h"
+#include "testsupport.h"
+
+int main()
+{
+    try
+    {
+        kealib::KEADataType keatype = CTypeStringToKEAType(STRINGIFY(KEA_DTYPE));
+    
+        kealib::KEAImageIO io;
+
+        std::string test_kea_file = "test_" STRINGIFY(KEA_DTYPE) ".kea";
+        
+        std::cout << "Opening file" << std::endl;
+        H5::H5File *h5file = kealib::KEAImageIO::openKeaH5RDOnly(test_kea_file);
+        io.openKEAImageHeader(h5file);
+        std::cout << "Opened file" << std::endl;
+        
+        auto spatialInfo2 = getSpatialInfo(10);
+        auto readinfo2 = io.getSpatialInfo();
+        if( !compareSpatialInfo(&spatialInfo2, readinfo2))
+        {
+            return 1;
+        }
+
+        std::cout << "Reading some image data" << std::endl;
+        KEA_DTYPE *pReadData = (KEA_DTYPE*)calloc(readinfo2->xSize * readinfo2->ySize, sizeof(KEA_DTYPE));
+        io.readImageBlock2Band(1, pReadData, 0, 0, readinfo2->xSize, readinfo2->ySize, readinfo2->xSize, readinfo2->ySize, keatype);
+        std::cout << "Read some image data" << std::endl;
+
+        KEA_DTYPE *pCheckData = createDataForType<KEA_DTYPE>(readinfo2->xSize, readinfo2->ySize);
+        std::cout << "Comparing data written and read data" << std::endl;
+        if( !compareData<KEA_DTYPE>(pReadData, pCheckData, readinfo2->xSize, readinfo2->ySize))
+        {
+            return 1;
+        }
+        std::cout << "Data compared" << std::endl;
+        
+        std::cout << "Reading a Subset" << std::endl;
+        KEA_DTYPE *pSubData = (KEA_DTYPE*)calloc(100 * 100, sizeof(KEA_DTYPE));
+        io.readImageBlock2Band(1, pSubData, 200, 150, 100, 100, 100, 100, keatype);
+        if( !compareDataSubset<KEA_DTYPE>(pReadData, pSubData, 200, 150, readinfo2->xSize, readinfo2->ySize, 100, 100))
+        {
+            return 1;
+        }
+        std::cout << "subset compared" << std::endl;
+        
+        free(pSubData);
+        free(pReadData);
+        free(pCheckData);
+        
+        if( io.maskCreated(1) )
+        {
+            std::cout << "Mask created" << std::endl;
+        }
+        else
+        {
+            std::cout << "Mask NOT created" << std::endl;
+            return 1;
+        }
+        
+        // note: mask always uint8_t in here
+        uint8_t *pReadMask = (uint8_t*)calloc(readinfo2->xSize * readinfo2->ySize, sizeof(uint8_t));
+        io.readImageBlock2BandMask(1, pReadMask, 0, 0, readinfo2->xSize, readinfo2->ySize, readinfo2->xSize, readinfo2->ySize, kealib::kea_8uint);
+        std::cout << "Read some mask data" << std::endl;
+
+        uint8_t *pMaskData = createDataForType<uint8_t>(readinfo2->xSize, readinfo2->ySize);
+
+        std::cout << "Comparing data written and read mask" << std::endl;
+        if( !compareData<uint8_t>(pReadMask, pMaskData, readinfo2->xSize, readinfo2->ySize))
+        {
+            return 1;
+        }
+        std::cout << "Mask compared" << std::endl;
+
+        if( io.getImageMetaData("Test1") != "Value1" )
+        {
+            std::cout << "Error reading metadata" << std::endl;
+            return 1;
+        }
+        if( io.getImageMetaData("Test2") != "Value2" )
+        {
+            std::cout << "Error reading metadata" << std::endl;
+            return 1;
+        }
+        
+        auto names = io.getImageMetaDataNames();
+        if( names.size() != 4 )
+        {
+            std::cout << "Wrong number of image metadata items" << std::endl;
+            return 1;
+        }
+        if( std::find(names.begin(), names.end(), "Test1") == names.end())
+        {
+            std::cout << "Can't find Test1 in image metadata" << std::endl;
+            return 1;
+        }
+        if( std::find(names.begin(), names.end(), "Test2") == names.end())
+        {
+            std::cout << "Can't find Test2 in image metadata" << std::endl;
+            return 1;
+        }
+        
+        auto name_values = io.getImageMetaData();
+        if( name_values.size() != 4 )
+        {
+            std::cout << "Wrong number of image metadata items/values" << std::endl;
+            return 1;
+        }
+        for( auto itr = name_values.begin(); itr != name_values.end(); itr++)
+        {
+            auto name = (*itr).first;
+            if( (name != "Test1") && (name != "Test2") && (name != "Test98") && (name != "Test99"))
+            {
+                std::cout << "Unexpected name " << name << std::endl;
+                return 1;
+            }
+            auto value = (*itr).second;
+            if( ( value != "Value1") && (value != "Value2") && (value != "Value98") && (value != "Value99"))
+            {
+                std::cout << "Unexpected name " << value << std::endl;
+                return 1;
+            }
+        }
+        std::cout << "Successfully read dataset metadata" << std::endl;
+
+        if( io.getImageBandMetaData(1, "BandTest1") != "Value1" )
+        {
+            std::cout << "Error reading metadata" << std::endl;
+            return 1;
+        }
+        if( io.getImageBandMetaData(1, "BandTest2") != "Value2" )
+        {
+            std::cout << "Error reading metadata" << std::endl;
+            return 1;
+        }
+
+        auto bandnames = io.getImageBandMetaDataNames(1);
+        if( bandnames.size() != 4 )
+        {
+            std::cout << "Wrong number of band metadata items" << std::endl;
+            return 1;
+        }
+        if( std::find(bandnames.begin(), bandnames.end(), "BandTest1") == names.end())
+        {
+            std::cout << "Can't find BandTest1 in image metadata" << std::endl;
+            return 1;
+        }
+        if( std::find(bandnames.begin(), bandnames.end(), "BandTest2") == names.end())
+        {
+            std::cout << "Can't find BandTest2 in image metadata" << std::endl;
+            return 1;
+        }
+        
+        auto bandname_values = io.getImageBandMetaData(1);
+        if( bandname_values.size() != 4 )
+        {
+            std::cout << "Wrong number of image metadata items/values" << std::endl;
+            return 1;
+        }
+        for( auto itr = bandname_values.begin(); itr != bandname_values.end(); itr++)
+        {
+            auto name = (*itr).first;
+            if( (name != "BandTest1") && (name != "BandTest2") && (name != "BandTest98") && (name != "BandTest99"))
+            {
+                std::cout << "Unexpected name " << name << std::endl;
+                return 1;
+            }
+            auto value = (*itr).second;
+            if( ( value != "Value1") && (value != "Value2") && (value != "Value98") && (value != "Value99"))
+            {
+                std::cout << "Unexpected value " << value << std::endl;
+                return 1;
+            }
+        }
+
+        std::cout << "Successfully read band metadata" << std::endl;
+
+        auto desc1 = io.getImageBandDescription(1);
+        auto desc2 = io.getImageBandDescription(2);
+        if( (desc1 != "HBand1Desc") || (desc2 != "HBand2Desc"))
+        {
+            std::cout << "band descriptions did not match " << desc1 << " " << desc1 << std::endl;
+            return 1;
+        }
+        std::cout << "Read band descriptions ok" << std::endl;
+        
+        // read nodata back as different type
+        uint16_t u32nodata = 0;
+        io.getNoDataValue(1, &u32nodata, kealib::kea_16uint);
+        if( u32nodata != 99 )
+        {
+            std::cout << "Wrong nodata value read " << u32nodata << std::endl;
+            return 1;
+        }
+        // band 2 nodata is undefined
+        double dnodata = -1;
+        bool bNoDataSet = true;
+        try
+        {
+            io.getNoDataValue(2, &dnodata, kealib::kea_64float);
+        }
+        catch(const kealib::KEAIOException &e)
+        {
+            bNoDataSet = false;
+        }
+        if( bNoDataSet )
+        {
+            std::cout << "Nodata not properly undefined " << dnodata << std::endl;
+            return 1;
+        }
+        std::cout << "Read nodata ok" << std::endl;
+        
+        if( io.getGCPProjection() != "KmKmqw" )
+        {
+            std::cout << "GCP Projection not correctly read" << std::endl;
+            return 1;
+        }
+        
+        if( io.getGCPCount() != 2 )
+        {
+            std::cout << "Wrong number of GCPs read" << std::endl;
+            return 1;
+        }
+        
+        auto readgcps = io.getGCPs();
+        auto pGCPs = getGCPData();
+        if(!compareGCPs(pGCPs, readgcps))
+        {
+            std::cout << "GCPs don't match" << std::endl;
+            return 1;
+        }
+        freeGCPData(pGCPs);
+        freeGCPData(readgcps);
+        
+        if( io.getAttributeTableChunkSize(1) != kealib::KEA_ATT_CHUNK_SIZE )
+        {
+            std::cout << "Attribute chunk size does not match" << std::endl;
+            return 1;
+        }
+        
+        if( io.getNumOfImageBands() != 2)
+        {
+            std::cout << "Wrong number of image bands" << std::endl;
+            return 1; 
+        }
+        
+        if( io.getImageBlockSize(1) != 512 ) 
+        {
+            std::cout << "image block size does not match" << std::endl;
+            return 1;
+        }
+        
+        // band 2 should be the default (kea_continuous)
+        // but we changed band 1
+        if( (io.getImageBandLayerType(1) != kealib::kea_continuous) ||
+            (io.getImageBandLayerType(2) != kealib::kea_thematic) )
+        {
+            std::cout << "layer types do not match" << std::endl;
+            return 1;
+        }
+        
+        // band 2 should be the default (kea_generic) but 
+        // we changed band 1
+        if( ( io.getImageBandClrInterp(1) != kealib::kea_generic) ||
+            (io.getImageBandClrInterp(2) != kealib::kea_redband))
+        {
+            std::cout << "colour interp does not match" << std::endl;
+            return 1;
+        }
+
+        // overviews        
+        if( io.getNumOfOverviews(1) != 2 )
+        {
+            std::cout << "should be 2 overviews" << std::endl;
+            return 1;
+        }
+        if(io.getNumOfOverviews(2) != 0)
+        {
+            std::cout << "should be 0 overviews" << std::endl;
+            return 1;
+        }
+        if( io.getOverviewBlockSize(1, 1) != OV_XSIZE)
+        {
+            std::cout << "wrong overview block size" << std::endl;
+            return 1;
+        }
+        
+        uint64_t xsize, ysize;
+        io.getOverviewSize(1, 1, &xsize, &ysize);
+        if( (xsize != OV_XSIZE) || (ysize != OV_YSIZE))
+        {
+            std::cout << "wrong overview size" << std::endl;
+            return 1;
+        }
+        io.getOverviewSize(1, 2, &xsize, &ysize);
+        if( (xsize != OV2_XSIZE) || (ysize != OV2_YSIZE))
+        {
+            std::cout << "wrong overview size2" << std::endl;
+            return 1;
+        }
+        
+        std::cout << "Reading some overview data" << std::endl;
+        KEA_DTYPE *pReadOvData = (KEA_DTYPE*)calloc(OV_XSIZE * OV_YSIZE, sizeof(KEA_DTYPE));
+        io.readFromOverview(1, 1, pReadOvData, 0, 0, OV_XSIZE, OV_YSIZE, OV_XSIZE, OV_YSIZE, keatype);
+        std::cout << "Read some overview data" << std::endl;
+
+        std::cout << "Comparing overview written and read data" << std::endl;
+        KEA_DTYPE *pTestOvData = createDataForType<KEA_DTYPE>(OV_XSIZE, OV_YSIZE);
+        if( !compareData<KEA_DTYPE>(pReadOvData, pTestOvData, OV_XSIZE, OV_YSIZE))
+        {
+            return 1;
+        }
+        free(pTestOvData);
+        free(pReadOvData);
+        std::cout << "Overview compared" << std::endl;
+        
+        io.close();
+    }
+    catch(const kealib::KEAException &e)
+    {
+        fprintf(stderr, "Exception raised: %s\n", e.what());
+        return 1;
+    }
+    printf("Success\n");
+
+    return 0;
+}
---
+++ src/tests/testwrite.cpp
@@ -0,0 +1,259 @@
+/*
+ *  testwrite.cpp
+ *  LibKEA
+ *
+ *  Created by Pete Bunting on 02/07/2012.
+ *  Copyright 2012 LibKEA. All rights reserved.
+ *
+ *  This file is part of LibKEA.
+ *
+ *  Permission is hereby granted, free of charge, to any person 
+ *  obtaining a copy of this software and associated documentation 
+ *  files (the "Software"), to deal in the Software without restriction, 
+ *  including without limitation the rights to use, copy, modify, 
+ *  merge, publish, distribute, sublicense, and/or sell copies of the 
+ *  Software, and to permit persons to whom the Software is furnished 
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be 
+ *  included in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+ *  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR 
+ *  ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+ *  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+ *  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include "libkea/KEAImageIO.h"
+#include "testsupport.h"
+
+int main()
+{
+    try
+    {
+        kealib::KEADataType keatype = CTypeStringToKEAType(STRINGIFY(KEA_DTYPE));
+    
+        auto spatialInfo = getSpatialInfo(0);
+    
+        auto bandDescrips = std::vector<std::string>(2);
+        bandDescrips[0] = "Band 1 Name";
+        bandDescrips[1] = "Band 2 Name";
+
+        kealib::KEAImageIO io;
+
+        std::string test_kea_file = "test_" STRINGIFY(KEA_DTYPE) ".kea";
+
+        std::cout << "Creating file" << std::endl;
+        H5::H5File *h5file = kealib::KEAImageIO::createKEAImage(test_kea_file,
+                        keatype, IMG_XSIZE, IMG_YSIZE, 2,
+                        &bandDescrips, &spatialInfo);
+        std::cout << "Created file" << std::endl;
+
+        delete h5file;
+
+
+        if (kealib::KEAImageIO::isKEAImage(test_kea_file))
+        {
+            std::cout << "File is a KEA image" << std::endl;
+        }
+        else
+        {
+            std::cout << "File is NOT a KEA image" << std::endl;
+            return 1;  
+        }
+
+        std::cout << "Opening file" << std::endl;
+        h5file = kealib::KEAImageIO::openKeaH5RW(test_kea_file);
+        std::cout << "Opened file" << std::endl;
+
+        io.openKEAImageHeader(h5file);
+        
+        auto readinfo = io.getSpatialInfo();
+        if( !compareSpatialInfo(&spatialInfo, readinfo))
+        {
+            return 1;
+        }
+        
+        auto spatialInfo2 = getSpatialInfo(10);
+        io.setSpatialInfo(&spatialInfo2);
+
+        uint64_t subXSize = IMG_XSIZE;
+        uint64_t subYSize = IMG_YSIZE;
+
+        uint64_t subXOff = 0;
+        uint64_t subYOff = 0;
+
+        std::cout << "Writing some image data" << std::endl;
+        KEA_DTYPE *pData = createDataForType<KEA_DTYPE>(subXSize, subYSize);
+        io.writeImageBlock2Band(1, pData, subXOff, subYOff, subXSize, subYSize,
+                    subXSize, subYSize, keatype);
+        free(pData);
+        std::cout << "Written some image data" << std::endl;
+        
+        io.createMask(1, 1);
+
+        std::cout << "Created Mask" << std::endl;
+                
+        // write some mask data - note always uint8_t here
+        uint8_t *pMaskData = createDataForType<uint8_t>(subXSize, subYSize);
+        io.writeImageBlock2BandMask(1, pMaskData, subXOff, subYOff, subXSize, subYSize,
+                    subXSize, subYSize, kealib::kea_8uint);
+        free(pMaskData);
+        std::cout << "Wrote mask" << std::endl;
+                    
+        // dataset metadata
+        io.setImageMetaData("Test1", "Value1");
+        io.setImageMetaData("Test2", "Value2");
+        
+        std::vector< std::pair<std::string, std::string> > metaData;
+        metaData.push_back(std::pair<std::string, std::string>("Test98","Value98"));
+        metaData.push_back(std::pair<std::string, std::string>("Test99","Value99"));
+        io.setImageMetaData(metaData);
+        std::cout << "Wrote dataset metadata" << std::endl;
+        
+        // band metadata
+        io.setImageBandMetaData(1, "BandTest1", "Value1");
+        io.setImageBandMetaData(1, "BandTest2", "Value2");
+
+        std::vector< std::pair<std::string, std::string> > bandmetaData;
+        bandmetaData.push_back(std::pair<std::string, std::string>("BandTest98","Value98"));
+        bandmetaData.push_back(std::pair<std::string, std::string>("BandTest99","Value99"));
+        io.setImageBandMetaData(1, bandmetaData);
+        std::cout << "Wrote band metadata" << std::endl;
+        
+        // description
+        // test what was set on create
+        if( (io.getImageBandDescription(1) != "Band 1 Name") ||
+            (io.getImageBandDescription(2) != "Band 2 Name") )
+        {
+            std::cout << "Band names set on creation not retriived" << std::endl;
+            return 1;
+        }
+        io.setImageBandDescription(1, "HBand1Desc");
+        io.setImageBandDescription(2, "HBand2Desc");
+        std::cout << "Wrote descriptions" << std::endl;
+        
+        // nodata
+        bool bNoDataSet = true;
+        uint16_t u16nodata = 99;
+        try
+        {
+            // check reading is empty
+            io.getNoDataValue(1, &u16nodata, kealib::kea_16uint);
+        }
+        catch(const kealib::KEAIOException &e)
+        {
+            bNoDataSet = false;
+        }
+        if( bNoDataSet )
+        {
+            std::cout << "Nodata appears set" << std::endl;
+            return 1;
+        }
+        if( u16nodata != 99 )
+        {
+            std::cout << "Nodata changed when not set" << std::endl;
+            return 1;
+        }
+        
+        io.setNoDataValue(1, &u16nodata, kealib::kea_16uint);
+        double dnodata = -123.89;
+        KEA_DTYPE expected_nodata = dnodata; // should cast to type of image
+        io.setNoDataValue(2, &dnodata, kealib::kea_64float);
+        io.getNoDataValue(2, &dnodata, kealib::kea_64float);
+        if( dnodata != expected_nodata )
+        {
+            std::cout << "Nodata not written correctly" << std::endl;
+            return 1;
+        }
+        io.undefineNoDataValue(2);
+        std::cout << "Wrote nodata" << std::endl;
+
+        auto pGCPs = getGCPData();
+
+        io.setGCPs(pGCPs, "WKT1");
+        std::cout << "wrote gcps" << std::endl;
+        
+        if( io.getGCPProjection() != "WKT1" )
+        {
+            std::cout << "GCP Projection not correctly read" << std::endl;
+            return 1;
+        }
+        
+        if( io.getGCPCount() != pGCPs->size() )
+        {
+            std::cout << "Wrong number of GCPs read" << std::endl;
+            return 1;
+        }
+        freeGCPData(pGCPs);
+        
+        io.setGCPProjection("KmKmqw");
+        std::cout << "Wrote GCP Proj" << std::endl;
+        
+        if ( (io.getImageBandDataType(1) != keatype) ||
+             (io.getImageBandDataType(2) != keatype))
+        {
+            std::cout << "Wrong data type read" << std::endl;
+            return 1;
+        }
+        
+        if( io.getKEAImageVersion() != "1.1")
+        {
+            std::cout << "wrong kealib version string" << std::endl;
+            return 1;
+        }
+        
+        io.setImageBandLayerType(2, kealib::kea_thematic);
+        std::cout << "set layer type" << std::endl;
+        
+        io.setImageBandClrInterp(2, kealib::kea_redband);
+        std::cout << "set layer colour interp" << std::endl;
+        
+        if( (io.getNumOfOverviews(1) != 0) || (io.getNumOfOverviews(2) != 0))
+        {
+            std::cout << "should be zero overviews" << std::endl;
+            return 1; 
+        }
+        
+        io.createOverview(1, 1, OV_XSIZE, OV_YSIZE);
+        io.createOverview(1, 2, OV2_XSIZE, OV2_YSIZE);
+        if( io.getNumOfOverviews(1) != 2 )
+        {
+            std::cout << "should be 2 overviews" << std::endl;
+            return 1;
+        }
+       
+        io.createOverview(2, 1, 50, 100);
+        io.removeOverview(2, 1);
+        if( io.getNumOfOverviews(2) != 0 )
+        {
+            std::cout << "should be 2 overviews" << std::endl;
+            return 1;
+        }
+        
+        // write to overview
+        KEA_DTYPE *pOvData = createDataForType<KEA_DTYPE>(OV_XSIZE, OV_YSIZE);
+        std::cout << "write to overview" << std::endl;
+        io.writeToOverview(1, 1, pOvData, 0, 0, OV_XSIZE, OV_YSIZE, OV_XSIZE, OV_YSIZE, keatype);
+        free(pOvData);
+        
+        io.close();
+        
+    }
+    catch(const kealib::KEAException &e)
+    {
+        fprintf(stderr, "Exception raised: %s\n", e.what());
+        return 1;
+    }
+    printf("Success\n");
+
+    return 0;
+}
+     
